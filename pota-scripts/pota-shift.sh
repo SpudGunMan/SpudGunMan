cd5f32135ea27c7173b138101d45a731bd9e9588
SpudGunMan <kellykeeton@yahoo.com>
2023-09-04 19:00:41 -0700

    Update pota-shift.sh

--- pota-scripts/pota-shift.sh
+++ pota-scripts/pota-shift.sh
@@ -3,22 +3,17 @@
 # Calculate POTA shift (Early/Day/Late)
 # This script will calculate the current POTA shift based on the current time in UTC
 # MIT License Copyright 2023 Kelly Keeton K7MHI
+# version 1.0.0
 
 # set variables for shift times by looking at the POTA Shifts Map and converting to local time
 # Shifts map https://docs.pota.app/assets/images/shift_map.png
-# "PST" time zone shown below in 24 hour format, shift time do not follow time zone see map
+# shown below in 24 hour format, shift time as viewed on the park time zone map
 early_start_local=0300
 early_end_local=0900
 late_start_local=1700
 
-# example of "EST" time zone shown below in 24 hour format
-#early_start_local=0600
-#early_end_local=1200
-#late_start_local=2200
-
 # Get the current time in local time
 local_time=$(date +%H%M)
-
 
 # Check if the current time is between the early shift
 if [ $local_time -ge $early_start_local ] && [ $local_time -lt $early_end_local ]; then
@@ -32,6 +27,10 @@ if [ $local_time -ge $late_start_local ]; then
     exit 0
 fi
 
-# If the current time is not in the early or late shift then
-echo "POTA Day Shift"
-exit 0
+If [ $local_time -gt $early_end_local ] && [ $local_time -lt $late_start_local ]; then
+    echo "POTA Day Shift"
+    exit 0
+fi
+
+echo "POTA Shift Unknown, like what happened..."
+exit 1
